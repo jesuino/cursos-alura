ea883c7422994fb7eeb752c7d6760ed95e962dd6
William Antonio <william.fatecsjc@gmail.com>
2016-03-03 03:45:14 -0300

    Começando curso XML

---
+++ xml/src/main/java/org/william/Principal.java
@@ -0,0 +1,37 @@
+package org.william;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+public class Principal {
+	
+	
+	public static void main(String[] args) throws Exception {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		Document document = builder.parse(Principal.class.getResourceAsStream("/vendas.xml"));
+		
+		Element venda = document.getDocumentElement();
+		NodeList list = document.getElementsByTagName("formaPagamento");
+		Element formaPagamento = (Element) list.item(0);
+		
+		System.out.println("Moeda: " + venda.getAttribute("moeda"));
+		System.out.println("Forma de pagamento: " + formaPagamento.getTextContent());
+		NodeList produtos = document.getElementsByTagName("produto");
+		
+		System.out.println("Produtos: ");
+		for (int i = 0; i < produtos.getLength(); i++) {
+			Element produtoEl = (Element) produtos.item(i);
+			String nome = produtoEl.getElementsByTagName("nome").item(0).getTextContent();
+			String precoStr = produtoEl.getElementsByTagName("preco").item(0).getTextContent();
+			Double preco = Double.parseDouble(precoStr);
+			Produto produto = new Produto(nome, preco);
+			System.out.println("    " + produto);
+		}
+	}
+
+}
---
+++ xml/src/main/java/org/william/Produto.java
@@ -0,0 +1,26 @@
+package org.william;
+
+public class Produto {
+
+	private String nome;
+	private double preco;
+
+	public Produto(String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+	
+	@Override
+	public String toString() {
+		return "Nome: "  + nome + ", preço: " + preco;
+	}
+	
+}
