3591e7cead36478595e769e20ab1679559131871
William Antonio <william.fatecsjc@gmail.com>
2016-03-08 04:39:08 -0300

    Melhorias no consumidor

--- jms-activemq/src/org/william/TesteConsumidor.java
+++ jms-activemq/src/org/william/TesteConsumidor.java
@@ -1,24 +1,34 @@
 package org.william;
 
+import java.util.Scanner;
+
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
-import javax.jms.Message;
 import javax.jms.MessageConsumer;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 
 public class TesteConsumidor {
+	@SuppressWarnings("resource")
 	public static void main(String[] args) throws Exception {
 		InitialContext ctx = new InitialContext();
 		ConnectionFactory factory = (ConnectionFactory) ctx.lookup("ConnectionFactory");
 		Connection conn = factory.createConnection();
 		conn.start();
 		Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
-		Destination destination =  (Destination) ctx.lookup("financeiro");
+		Destination destination = (Destination) ctx.lookup("financeiro");
 		MessageConsumer consumer = session.createConsumer(destination);
-		Message msg = consumer.receive();
-		System.out.println("Mensagem: " + msg);
+		consumer.setMessageListener(m -> {
+			TextMessage txtMessage = (TextMessage) m;
+			try {
+				System.out.println(txtMessage.getText());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+		new Scanner(System.in).nextLine();
 		session.close();
 		conn.close();
 		ctx.close();
