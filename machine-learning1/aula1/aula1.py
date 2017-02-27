e28f3a5a0ed6c370ca96dc349354176a20ddd29b
jesuino <wsiqueir@redhat.com>
2017-02-26 23:17:20 -0300

    Add aula de machine learning

---
+++ machine-learning1/aula1/aula1.py
@@ -0,0 +1,32 @@
+porco1 = [1, 1, 0]
+porco2 = [1, 1, 0]
+porco3 = [1, 1, 0]
+
+cachorro4 = [1, 1, 1]
+cachorro5 = [0, 1, 1]
+cachorro6 = [0, 1, 1]
+
+dados = [porco1, porco2, porco3,
+	cachorro4, cachorro5, cachorro6]
+
+marcacoes = [1,1,1,-1,-1,-1]
+
+from sklearn.naive_bayes import MultinomialNB
+
+algoritmo = MultinomialNB()
+algoritmo.fit(dados, marcacoes)
+
+teste = [
+	[1,0,0],
+	[0,0,0],
+	[1,1,1]
+]
+marcacoes_teste = [1, -1, 1]
+resultado = algoritmo.predict(teste)
+diferenca = resultado - marcacoes_teste
+total_acertos = len([ a for a in diferenca if a == 0])
+total_testes = len(teste)
+print total_acertos,  total_testes
+taxa_acertos = 100.0 * (total_acertos / float(total_testes))
+print resultado 
+print "Total de acertos: ", taxa_acertos
